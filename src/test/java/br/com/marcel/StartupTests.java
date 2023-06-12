1bb32e9d926b35bcc68671b3ca1c0b6124b58688
Marcel Philippe Abreu Andrade <marcelpaa@hotmail.com>
2023-06-12 16:34:10 -0300

    Comite inicial do projeto.

---
+++ src/test/java/br/com/marcel/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.marcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
